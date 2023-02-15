271370bfb688ea4d2c253aecd8aef9785625be60
David Casanellas <davidcasanell@gmail.com>
2023-02-16 00:03:33 +0100

    Add server_port and server_protocol vars and output public_ip and server_port

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "server_protocol" {
+  description = "The protocol the server will use for HTTP requests"
+  type        = string
+  default     = "tcp"
+}
+
 terraform {
   required_providers {
     aws = {
@@ -19,7 +31,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, world!" > index.html
-              nohup busybox httpd -f -p 8080 & 
+              nohup busybox httpd -f -p ${var.server_port} & 
               EOF
 
   user_data_replace_on_change = true
@@ -33,11 +45,20 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = var.server_protocol
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
+
+output "server_port" {
+  value       = var.server_port
+  description = "The port used for HTTP requests"
+}
 
