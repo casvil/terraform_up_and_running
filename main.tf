d9064cc4a4d6ad57b8deb06c5f2953d9cd3ed2aa
David Casanellas <davidcasanell@gmail.com>
2023-02-16 01:03:26 +0100

    Deploy a cluster of web servers. Use aws auto scaling group and aws launch configuration replacing aws single instance

--- main.tf
+++ main.tf
@@ -10,6 +10,17 @@ variable "server_protocol" {
   default     = "tcp"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 terraform {
   required_providers {
     aws = {
@@ -23,10 +34,10 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -34,11 +45,25 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} & 
               EOF
 
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "terraform-example"
+  # Required when using a launch configuration with an autoscaling group
+  lifecycle {
+    create_before_destroy = true
   }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
 }
 
 resource "aws_security_group" "instance" {
@@ -52,13 +77,13 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
-}
+# output "public_ip" {
+#   value       = aws_instance.example.public_ip
+#   description = "The public IP address of the web server"
+# }
 
-output "server_port" {
-  value       = var.server_port
-  description = "The port used for HTTP requests"
-}
+# output "server_port" {
+#   value       = var.server_port
+#   description = "The port used for HTTP requests"
+# }
 
