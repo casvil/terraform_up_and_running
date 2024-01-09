ab3a9af06153f50225560336e2685d61db687117
David Casanellas <davidcasanell@gmail.com>
2024-01-09 21:52:00 +0100

    Rename s3 bucket and dynamodb

--- chapter3/main.tf
+++ chapter3/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-up-and-running-state"
+  bucket = "casvil-terraform-up-and-running-state"
 
   # Prevent accidental deletion of this S3 bucket
   lifecycle {
@@ -41,7 +41,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
+  name         = "casvil-terraform-up-and-running-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
